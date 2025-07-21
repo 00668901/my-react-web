fd57a6ba2e53d8b5b1af5c7292f2501104a711fd
Steven Kurniawan Haryanto <152142933+00668901@users.noreply.github.com>
2025-07-21 13:05:47 +0700

    Ready for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
- root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
